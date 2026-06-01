8edbd287c12eca26090e90305ed3da56d091eb95
Marcelo Ramirez <marceloramirezzz1@gmail.com>
2026-06-01 16:20:46 -0300

    Sample Data added

---
+++ app/(app)/page.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      {/* Featured Products Carousel */}
+      <Button>Click me</Button>
+      <Button variant="outline">Click me</Button>
+      <Button variant="secondary">Click me</Button>
+      <Button variant="ghost">Click me</Button>
+      <Button variant="destructive">Click me</Button>
+      <Button variant="link">Click me</Button>
+
+
+      {/* Page Banner */}
+
+      {/* Category Tiles */}
+
+      {/* Products Section */}
+    </div>
+  );
+}
