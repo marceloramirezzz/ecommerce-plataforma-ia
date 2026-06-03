e1d0a39adc40316d780d463a798d4e242df1b693
Marcelo Ramirez <marceloramirezzz1@gmail.com>
2026-06-03 13:00:07 -0300

    Carousel y Banner

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,9 +1,11 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { SanityLive } from "@/sanity/lib/live";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <main>{children}</main>
+      <SanityLive />
     </ClerkProvider>
   );
 }
